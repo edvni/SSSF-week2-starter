624ff515517b1d11ce66cb14a25dd6370e5ea8e6
Edvard Nivala <edvard.nivala@metropolia.fi>
2024-02-10 18:48:58 +0200

    Adjusted to match .env

--- test/api.test.ts
+++ test/api.test.ts
@@ -33,7 +33,7 @@ interface UserWithToken {
 
 describe('GET /api/v1', () => {
   beforeAll(async () => {
-    await mongoose.connect(process.env.DATABASE_URL as string);
+    await mongoose.connect(process.env.DB_URL as string);
   });
 
   afterAll(async () => {
